b5cb3b951b8c0248064314169c278f6e56171e0c
Игорь Гордеев <igor@MacBook-Pro-Igor.local>
2024-08-18 18:24:51 +0300

    тестовый измения файла db.js добавлены параметры

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect("mongodb://79.174.86.232:27017/layouts", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
     console.log("mongoDB layouts connected");
   } catch (error) {
