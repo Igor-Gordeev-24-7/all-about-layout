e61239ad75728eb7af483809e73e6632fe4171bb
Игорь Гордеев <igor@MacBook-Pro-Igor.local>
2024-08-21 18:20:56 +0300

    добавление helmet в index.js

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,16 @@
-import fs from 'fs';
-import path from 'path';
-import https from 'https';
-import express from 'express';
-import cors from 'cors';
+import fs from "fs";
+import path from "path";
+import https from "https";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
 
-import layoutsRouter from './routes/layouts.js';
-import articlesRouter from './routes/articles.js';
-import membersRouter from './routes/members.js';
-import connectDB from './config/db.js';
-import connectDBArticles from './config/dbArticles.js';
-import connectDBMembers from './config/dbMembers.js';
+import layoutsRouter from "./routes/layouts.js";
+import articlesRouter from "./routes/articles.js";
+import membersRouter from "./routes/members.js";
+import connectDB from "./config/db.js";
+import connectDBArticles from "./config/dbArticles.js";
+import connectDBMembers from "./config/dbMembers.js";
 
 // Создание Express приложения
 const app = express();
@@ -23,24 +24,26 @@ connectDBMembers();
 app.use(cors());
 app.use(express.json());
 
+app.use(require("helmet")());
+
 // Определение маршрутов
-app.use('/', layoutsRouter);
-app.use('/', articlesRouter);
-app.use('/', membersRouter);
+app.use("/", layoutsRouter);
+app.use("/", articlesRouter);
+app.use("/", membersRouter);
 
 // Путь к сертификатам
 const sslOptions = {
-  key: fs.readFileSync(path.join(__dirname, 'sslcert', 'privkey.pem')),
-  cert: fs.readFileSync(path.join(__dirname, 'sslcert', 'fullchain.pem'))
+  key: fs.readFileSync(path.join(__dirname, "sslcert", "privkey.pem")),
+  cert: fs.readFileSync(path.join(__dirname, "sslcert", "fullchain.pem")),
 };
 
 // Запуск HTTPS сервера
 https.createServer(sslOptions, app).listen(8443, () => {
-  console.log('HTTPS сервер запущен на порту 8443');
+  console.log("HTTPS сервер запущен на порту 8443");
 });
 
 // Опционально: Запуск HTTP сервера на порту 5001
 // Вы можете оставить или закомментировать эту часть, если вы не хотите использовать HTTP
 app.listen(5001, () => {
-  console.log('HTTP сервер запущен на порту 5001');
+  console.log("HTTP сервер запущен на порту 5001");
 });
