ad2b3ac267dcfc2849946c02dc0af43371f0e177
Игорь Гордеев <igor@MacBook-Pro-Igor.local>
2024-08-22 07:26:15 +0300

    изменен index.js прописаны пути ssl сертификата

--- backend/index.js
+++ backend/index.js
@@ -24,17 +24,18 @@ connectDBMembers();
 app.use(cors());
 app.use(express.json());
 
-app.use(require("helmet")());
+// Использование Helmet для базовой защиты
+app.use(helmet());
 
 // Определение маршрутов
 app.use("/", layoutsRouter);
 app.use("/", articlesRouter);
 app.use("/", membersRouter);
 
-// Путь к сертификатам
+// Путь к сертификатам, полученным через Certbot
 const sslOptions = {
-  key: fs.readFileSync(path.join(__dirname, "sslcert", "privkey.pem")),
-  cert: fs.readFileSync(path.join(__dirname, "sslcert", "fullchain.pem")),
+  key: fs.readFileSync("/etc/letsencrypt/live/79-174-86-232.cloudvps.regruhosting.ru/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/79-174-86-232.cloudvps.regruhosting.ru/fullchain.pem"),
 };
 
 // Запуск HTTPS сервера
@@ -43,7 +44,7 @@ https.createServer(sslOptions, app).listen(8443, () => {
 });
 
 // Опционально: Запуск HTTP сервера на порту 5001
-// Вы можете оставить или закомментировать эту часть, если вы не хотите использовать HTTP
+// Вы можете оставить или закомментировать эту часть, если не хотите использовать HTTP
 app.listen(5001, () => {
   console.log("HTTP сервер запущен на порту 5001");
 });
